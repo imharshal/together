53ee1f77a1f89f2eb4537f73e4d5a9f6bdce5ebb
Harshal <harshalbokade984@gmail.com>
2022-11-11 17:59:48 +0530

    vite config updated to run it in production

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// yarn add --dev @esbuild-plugins/node-globals-polyfill
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
-// yarn add --dev @esbuild-plugins/node-modules-polyfill
-import { NodeModulesPolyfillPlugin } from "@esbuild-plugins/node-modules-polyfill";
-// You don't need to add this to deps, it's included by @esbuild-plugins/node-modules-polyfill
-import rollupNodePolyFill from "rollup-plugin-node-polyfills";
-// import nodePolyfills from "rollup-plugin-polyfill-node";
 
-// # yarn
 // yarn add --dev vite-plugin-node-polyfills
 //socket.dev/npm/package/vite-plugin-node-polyfills
 import { nodePolyfills } from "vite-plugin-node-polyfills";
@@ -24,11 +16,6 @@ export default defineConfig({
       },
       // Enable esbuild polyfill plugins
       plugins: [
-        // NodeGlobalsPolyfillPlugin({
-        //   process: true,
-        //   buffer: true,
-        // }),
-        // NodeModulesPolyfillPlugin(),
         nodePolyfills({
           // Whether to polyfill `node:` protocol imports.
           protocolImports: true,
@@ -36,14 +23,4 @@ export default defineConfig({
       ],
     },
   },
-
-  // build: {
-  //   rollupOptions: {
-  //     plugins: [
-  //       // Enable rollup polyfills plugin
-  //       // used during production bundling
-  //       // rollupNodePolyFill(),
-  //     ],
-  //   },
-  // },
 });

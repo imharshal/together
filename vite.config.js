012e519e2585c730274488ebe6c263c6041137b0
Harshal <harshalbokade984@gmail.com>
2022-11-15 17:37:22 +0530

    polyfill issue resolved

--- vite.config.js
+++ vite.config.js
@@ -1,41 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// yarn add --dev vite-plugin-node-polyfills
-//socket.dev/npm/package/vite-plugin-node-polyfills
-// import { nodePolyfills } from "vite-plugin-node-polyfills";
-// import rollupNodePolyFill from "rollup-plugin-node-polyfills";
-
-import { NgmiPolyfill } from "vite-plugin-ngmi-polyfill";
+// yarn add rollup-plugin-node-polyfills
+import rollupNodePolyFill from "rollup-plugin-node-polyfills";
+// yarn add vite-plugin-node-stdlib-browser
+import nodePolyfills from "vite-plugin-node-stdlib-browser";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // plugins: [react()],
-  plugins: [react(), NgmiPolyfill()],
+  plugins: [react(), nodePolyfills()],
   optimizeDeps: {
     esbuildOptions: {
       // Node.js global to browser globalThis
       define: {
         global: "globalThis",
       },
-      // Enable esbuild polyfill plugins
-
-      // plugins: [
-      //   nodePolyfills({
-      //     // Whether to polyfill `node:` protocol imports.
-      //     protocolImports: true,
-      //   }),
-      // ],
     },
   },
-
-  // build: {
-  //   rollupOptions: {
-  //     plugins: [
-  //       // Enable rollup polyfills plugin
-  //       // used during production bundling
-  //       nodePolyfills(),
-  //     ],
-  //   },
-  // },
+  build: {
+    rollupOptions: {
+      plugins: [
+        // Enable rollup polyfills plugin
+        // used during production bundling
+        rollupNodePolyFill(),
+      ],
+    },
+  },
 });
